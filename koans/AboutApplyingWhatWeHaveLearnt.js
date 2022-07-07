af0013de116a6a85852fd84991ea777507f33425
Trey A <ta08annis@gmail.com>
2022-07-07 16:13:23 -0400

    All koans except AboutApplyingWhatWeHaveLearnt.js completed. Stuck on last exercise.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,16 +32,28 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
-      var productsICanEat = [];
+    var checkMushroomsAndNuts = function(product) {
+      if (product.ingredients.includes("mushrooms") || product.containsNuts) {
+        return false;
+      } else {
+        return true;
+      }
+  }
 
-      /* solve using filter() & all() / any() */
+    var containsNuts = function(product) {
+      return product.containNuts
+  }
+        /* solve using filter() & all() / any() */
+    //var productsWithoutMushrooms = products.filter(checkMushrooms);
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+    var productsICanEat = products.filter(checkMushroomsAndNuts);
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +67,26 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var tempArr = _.range(1, 1000);
+    var arr = [];
+    for (var i of tempArr) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        arr.push(i)
+      }
+    }
+    var sum = arr.reduce( function (sum, x) { return sum + x; })
 
-    expect(233168).toBe(FILL_ME_IN);
+
+/* try chaining range() and reduce() */
+
+/* try chaining range() and reduce() */
+
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,7 +99,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
